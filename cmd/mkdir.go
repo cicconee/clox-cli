868142f4fd3b836dcb8d6e474064a56648d9de3c
edward ciccone <ciccone.etp@gmail.com>
2024-01-31 09:53:54 -0600

    MkdirCommand calls the Clox API to create a new directory.

--- cmd/mkdir.go
+++ cmd/mkdir.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/cicconee/clox-cli/internal/config"
 	"github.com/cicconee/clox-cli/internal/crypto"
@@ -12,11 +17,8 @@ import (
 
 // The 'mkdir' command.
 //
-// mkdirCmd will create a new directory on the Clox server. The path flag is optional.
-// If not provided directory will default to the users root.
-//
-// TODO:
-//   - Call Clox API endpoint to create directory.
+// MkdirCommand will create a new directory on the Clox server. The path flag is
+// optional. If not provided directory will default to the users root.
 type MkdirCommand struct {
 	cmd      *cobra.Command
 	user     *config.User
@@ -58,24 +60,155 @@ func (c *MkdirCommand) SetPassword(password string) {
 	c.password = password
 }
 
+// NewDirRequest is the body of the POST request when creating a new directory.
+type NewDirRequest struct {
+	// The name of the directory being created.
+	Name string `json:"name"`
+}
+
+// NewDirResponse is the response body of the POST request when creating a new
+// directory.
+type NewDirResponse struct {
+	ID        string    `json:"id"`
+	OwnerID   string    `json:"owner_id"`
+	ParentID  string    `json:"parent_id"`
+	DirName   string    `json:"directory_name"`
+	DirPath   string    `json:"directory_path"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	LastWrite time.Time `json:"last_write"`
+}
+
+// ErrorResponse is the response body when the API server responds with an error.
+// Every error response from the server conforms to this structure.
+type ErrorResponse struct {
+	Err        string `json:"error"`
+	StatusCode int    `json:"status_code"`
+}
+
+// APIError is a custom error type that represents an HTTP error response from the
+// API.
+//
+// APIError satisfies the error interface.
+type APIError struct {
+	Err        string
+	StatusCode int
+}
+
+// The function that satisfies the error interface.
+func (e *APIError) Error() string {
+	return e.Err
+}
+
 // Run is the Run function of the cobra.Command in this InitCommand.
 //
 // Run will create a user and write it to the configuration file. If the
 // configuration directory does not exist it will create it. If the user is already
 // configured, it will print a message stating Clox CLI is already set up.
 func (c *MkdirCommand) Run(cmd *cobra.Command, args []string) {
-	fmt.Println("User:", c.user)
-	fmt.Println("Dir Name:", args[0])
-	fmt.Println("Path:", c.path)
-	fmt.Println("Password:", c.password)
-
 	token, err := c.user.APIToken(c.aes, c.password)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Token:", token)
+	data := &NewDirRequest{Name: args[0]}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error: Marshal:", err)
+		os.Exit(1)
+	}
 
-	os.Exit(0)
+	// Create the request, add the token to the authorization header, and set the
+	// "path" query parameter.
+	url := "http://localhost:8081/api/dir"
+	r, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error: Creating Request:", err)
+		os.Exit(1)
+	}
+	authHeader := fmt.Sprintf("Bearer %s", token)
+	r.Header.Set("Authorization", authHeader)
+	q := r.URL.Query()
+	q.Set("path", c.path)
+	r.URL.RawQuery = q.Encode()
+
+	// Create the HTTP client and do the request.
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Println("Error: Sending Request:", err)
+		os.Exit(1)
+	}
+
+	respData := &NewDirResponse{}
+	err = HandleResponse(resp, respData)
+	if err != nil {
+		switch e := err.(type) {
+		case *APIError:
+			fmt.Printf("API Error [%d]: %s\n", e.StatusCode, e.Err)
+			fmt.Printf("-> Name: %s\n", args[0])
+			fmt.Printf("-> Path: %s\n", c.path)
+			os.Exit(0)
+		default:
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Printf("API [%d]: Directory Created", resp.StatusCode)
+	fmt.Printf("-> Name: %s\n", respData.DirName)
+	fmt.Printf("-> Path: %s\n", respData.DirPath)
+	return
+}
+
+// HandleResponse handles http.Response from the Clox API. A successful request will
+// parse JSON body into dst.
+//
+// If the API responds with an error (non-200 status code), it will return an
+// *APIError.
+func HandleResponse(r *http.Response, dst any) error {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("reading body: %w", err)
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != 200 {
+		return parseErrorResponse(body, r.StatusCode)
+	}
+
+	return parseResponse(body, dst)
+}
+
+// parseResponse will unmarshal the JSON in the []byte into dst.
+//
+// The []byte is expected to hold a valid JSON structure, if it does not, an error
+// is returned.
+func parseResponse(b []byte, dst any) error {
+	err := json.Unmarshal(b, dst)
+	if err != nil {
+		return fmt.Errorf("unmarshalling body: %w", err)
+	}
+
+	return nil
+}
+
+// parseErrorResponse will unmarshal an API error response and return it as a
+// *APIError. The JSON in the []byte is unmarshalled into an ErrorResponse. The
+// ErrorResponse is then used to construct and return a *APIError.
+//
+// If unmarshalling the []byte fails, it will still return a *APIError, but the
+// Err field will specify that parsing the API error response failed. If this ever
+// happens, most likely the server is responding with invalid data and something is
+// wrong.
+func parseErrorResponse(b []byte, statusCode int) error {
+	var errResp ErrorResponse
+	if err := json.Unmarshal(b, &errResp); err != nil {
+		return &APIError{
+			StatusCode: statusCode,
+			Err:        "Failed to parse API error response"}
+	}
+
+	return &APIError{Err: errResp.Err, StatusCode: errResp.StatusCode}
 }
