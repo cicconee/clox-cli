33e23a04ceb0d44a8d9b609e3f185c5e4035fb2f
edward ciccone <ciccone.etp@gmail.com>
2024-02-04 11:42:41 -0600

    Abstracted common logic among API calls that create a directory.

--- internal/api/dir.go
+++ internal/api/dir.go
@@ -37,53 +37,43 @@ type newDirRequestBody struct {
 	Name string `json:"name"`
 }
 
-// NewDirWithPath calls the API to create a new directory. The path parameter is
-// the path that the directory will be created within. This parameter is optional
-// and if empty will create the directory in the users root directory on the server.
+// NewDirWithPath calls the API to create a new directory by specifying the parent
+// directory path. The path parameter is the path that the directory will be created
+// within. This parameter is optional and if empty will create the directory in the
+// users root directory on the server.
 //
 // If the API responds with an error (non-200 status code), it will return nil and
 // an *APIError.
 func NewDirWithPath(client *http.Client, path string, p NewDirParams) (*NewDirResponse, error) {
-	reqBody := newDirRequestBody{Name: p.DirName}
-	jsonData, err := json.Marshal(&reqBody)
-	if err != nil {
-		return nil, fmt.Errorf("marshalling data: %w", err)
-	}
-
-	req, err := NewRequest(RequestParams{
-		Method: "POST",
-		URL:    fmt.Sprintf("%s/api/dir", p.BaseURL),
-		Body:   bytes.NewBuffer(jsonData),
-		Token:  p.Token,
-		Query:  map[string]string{"path": path},
-	})
-	if err != nil {
-		return nil, fmt.Errorf("creating request: %w", err)
-	}
-	defer req.Body.Close()
-
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("sending request: %w", err)
-	}
-	defer res.Body.Close()
-
-	respData := &NewDirResponse{}
-	err = ParseResponse(res, respData)
-	if err != nil {
-		return nil, err
-	}
-
-	return respData, nil
+	return newDir(client, newDirConfig{
+		NewDirParams: p,
+		URLPath:      "api/dir",
+		Query:        map[string]string{"path": path}})
 }
 
-// NewDirWithID calls the API to create a new directory. The id parameter is the ID
-// of the directory that the new directory will be created in (the parent directory).
+// NewDirWithID calls the API to create a new directory by specifying the parent
+// directory ID. The id parameter is the ID of the directory that the new directory
+// will be created in (the parent directory).
 //
 // If the API responds with an error (non-200 status code), it will return nil and
 // an *APIError.
 func NewDirWithID(client *http.Client, id string, p NewDirParams) (*NewDirResponse, error) {
-	reqBody := newDirRequestBody{Name: p.DirName}
+	return newDir(client, newDirConfig{
+		NewDirParams: p,
+		URLPath:      fmt.Sprintf("api/dir/%s", id),
+	})
+}
+
+// newDirConfig is the configuration for calling the Clox API to create a directory.
+type newDirConfig struct {
+	NewDirParams
+	URLPath string
+	Query   map[string]string
+}
+
+// newDir creates a new directory by calling the Clox API.
+func newDir(client *http.Client, c newDirConfig) (*NewDirResponse, error) {
+	reqBody := newDirRequestBody{Name: c.DirName}
 	jsonData, err := json.Marshal(&reqBody)
 	if err != nil {
 		return nil, fmt.Errorf("marshalling data: %w", err)
@@ -91,9 +81,10 @@ func NewDirWithID(client *http.Client, id string, p NewDirParams) (*NewDirRespon
 
 	req, err := NewRequest(RequestParams{
 		Method: "POST",
-		URL:    fmt.Sprintf("%s/api/dir/%s", p.BaseURL, id),
+		URL:    fmt.Sprintf("%s/%s", c.BaseURL, c.URLPath),
 		Body:   bytes.NewBuffer(jsonData),
-		Token:  p.Token,
+		Token:  c.Token,
+		Query:  c.Query,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("creating request: %w", err)
